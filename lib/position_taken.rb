225e5ad4bac3ec0dfcbad74aa1fbba33c4703c98
Evan Delasota <evan.delasota@gmail.com>
2020-02-09 17:04:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   index -= 1
 
-  if (board[index] != " ")
+  if (board[index] != " " || board[index] == "X" || board[index] == "O")
     return true
   else
     return false
