d08d09eb365c886f53cdc9836493ef32cc809b71
Evan Delasota <evan.delasota@gmail.com>
2020-02-09 17:12:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  index -= 1
+  index = 1 + index.to_i
 
-  if (board[index] != " ")
+  if (board[index] == "X" || board[index] == "Y")
     return true
   else
     return false
